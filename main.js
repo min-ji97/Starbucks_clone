2b1d3a7c6f58b9990b1e9d2e4dd87eecc32e9491
jominji <whalswl576@gmail.com>
2022-04-22 03:13:49 +0900

    ( html+css+js ):container-스프링 시즌 원두: js로 애니메이션효과

--- main.js
+++ main.js
@@ -12,3 +12,48 @@ fadeIn.forEach(( item , index )=>{
         opacity: 1
     });
 });
+
+// 공지사항 및 프로모션
+// 
+const dd = document.querySelector('.promotion_box_add_btn');
+
+
+
+
+// 스타벅스 스프링 시즌 원두
+
+window.addEventListener("scroll",(e)=>{
+    let scrollY = this.scrollY;
+    if( scrollY >= 200){
+        gsap.to('.spring_season_blend_img img', 1.8, {
+            x: 969,
+            delay: 0.3,
+            opacity: 1
+        });
+        gsap.to('.spring_season_blend_txt', 1.8 , {
+            x: -1071,
+            delay: 0.3,
+            opacity: 1
+        });
+
+
+    }else if( scrollY < 200){ // 스크롤이 200 보다 작으면 다시 지워주기..!
+        console.log('200아래임..!');
+        gsap.to('.spring_season_blend_img img',{
+            x: -969,
+            opacity:0
+        });
+        gsap.to('.spring_season_blend_txt',{
+            x: 1071,
+            opacity:0
+        });
+        // let dd = document.querySelector('.spring_season_blend_img img');
+        // let ss = document.querySelector('spring_season_blend_txt');
+        // dd.style.left = '-800px';
+        // ss.style.left = '1700px';
+    }
+});
+//spring_season_blend_img
+
+
+
